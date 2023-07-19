ac6962c7c619b04f6b484efd1620ad1e2c0ea99e
Annie He <anniezhe0@gmail.com>
2023-07-19 16:28:49 -0400

    set index

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Navigation from './Navigation';
-import reportWebVitals from './reportWebVitals';
-import About from './pages/About';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Navigation from "./Navigation";
+import reportWebVitals from "./reportWebVitals";
+import About from "./pages/About";
 // import Contact from './pages/Contact';
 // import Footer from './pages/Footer';
 // import Project from './pages/Project';
-import Home from './pages/Home';
+import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-export default function App(){return(
-  <BrowserRouter>
-    <Navigation />
-    <Routes>
-    <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-    </Routes>
-  </BrowserRouter>
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
