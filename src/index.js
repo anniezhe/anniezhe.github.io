c4177e6a4583db2f9c6998c9a9eabeab3c0b4099
Annie He <anniezhe0@gmail.com>
2023-07-19 18:00:23 -0400

    root

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import React from "react";
-import render, { createRoot } from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Navigation from "./pages/Navigation";
 import About from "./pages/About";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-const container = document.getElementById("root");
-const root = createRoot(container)
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
 root.render(
-    <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
-    </Router>
+  <BrowserRouter>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
 );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
