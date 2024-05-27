ed08c55f5514024e30af3ccf0af2925834e088a1
Annie He <anniezhe0@gmail.com>
2024-05-27 13:15:12 -0400

    configure website path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/annie-website",
+    output: "export",  // <=== enables static exports
+reactStrictMode: true};
 
 export default nextConfig;
