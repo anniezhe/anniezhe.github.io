8374df58bfee8f44cc6440c8e5df32dae75a8a9e
Annie He <anniezhe0@gmail.com>
2024-10-10 21:56:33 -0400

    make my personal experience clearer

--- pages/index.tsx
+++ pages/index.tsx
@@ -75,7 +75,7 @@ export default function IndexPage() {
                     - rejected medical treatments due to health insurance limits
                     <br />
                     <br />
-                    In my personal experience, I had to wait 3 weeks to get a simple, life-saving medical treatment, which was not acceptable. 
+                    In my personal experience, I had to wait 3 weeks to get permission for a simple, life-saving medical treatment, which was not acceptable. 
                     <br />
                     <br />
                     My goal is to be a part of the solution by doing three things:
