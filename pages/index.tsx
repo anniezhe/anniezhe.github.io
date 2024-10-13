4f2bfa78ad719d4118c4baa933276551dd3167d6
Annie He <anniezhe0@gmail.com>
2024-10-13 19:33:06 -0400

    add my writing piece to project section

--- pages/index.tsx
+++ pages/index.tsx
@@ -124,14 +124,18 @@ export default function IndexPage() {
                     </TableHeader>
                     <TableBody>
                     <TableRow key="1">
+                        <TableCell><Link href="https://medium.com/@anniezhe0/general-overview-of-the-us-healthcare-system-12628979bdf9">General Overview of the US Healthcare System</Link></TableCell>
+                        <TableCell>October 2024</TableCell>
+                      </TableRow>
+                    <TableRow key="2">
                         <TableCell><Link href="https://github.com/anniezhe/cdc_botulism">Centers for Disease Control and Prevention (CDC)'s Botulism Report from 1899 to 2017</Link></TableCell>
                         <TableCell>September 2024</TableCell>
                       </TableRow>
-                      <TableRow key="2">
+                      <TableRow key="3">
                         <TableCell><Link href="https://github.com/anniezhe/cfpb_complaint_dataset">2022-2023 Consumer Financial Protection Bureau (CFPB) Complaint Report</Link></TableCell>
                         <TableCell>December 2023</TableCell>
                       </TableRow>
-                      <TableRow key="3">
+                      <TableRow key="4">
                         <TableCell><Link href="https://medium.com/@anniezhe0/using-random-forest-regression-model-on-the-mastercards-stock-data-itself-1a069ad9934a">Using Random Forest Regression Model on the Mastercard’s Stock Data Itself</Link></TableCell>
                         <TableCell>April 2023</TableCell>
                       </TableRow>
