0f9f482da380949ac29809176031263d788b0a97
Annie He <anniezhe0@gmail.com>
2024-11-12 12:41:59 -0500

    css for Materials section

---
+++ components/ListboxWrapper.jsx
@@ -0,0 +1,5 @@
+export const ListboxWrapper = ({children}) => (
+    <div className="w-full max-w-[500px] underline px-1 py-2 rounded-small border-default-200 dark:border-default-100">
+      {children}
+    </div>
+  );
