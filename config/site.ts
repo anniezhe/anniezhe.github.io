9f1f7ad5c82a948efa45d8737def825bbea8253a
Annie He <anniezhe0@gmail.com>
2024-11-26 17:06:45 -0500

    change Bluesky url

--- config/site.ts
+++ config/site.ts
@@ -65,6 +65,6 @@ export const siteConfig = {
     contact: "mailto:anniezhe0@gmail.com",
     linkedin: "https://linkedin.com/in/anniezhe0",
     medium: "https://medium.com/@anniezhe0/about",
-    bluesky: "https://bsky.app/profile/anniezhe0.bsky.social",
+    bluesky: "https://bsky.app/profile/anniezhe.com",
   },
 };
