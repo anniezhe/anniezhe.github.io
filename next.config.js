c1f1ad6eb03f59893fb329502977cae1991e1862
Annie He <anniezhe0@gmail.com>
2026-05-23 06:46:38 -0400

    enforce normal module replacement plugin

--- next.config.js
+++ next.config.js
@@ -1,17 +1,29 @@
+const webpack = require('webpack');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: { unoptimized: true },
   
-  // 1. Core NextUI transpile configurations
+  // 1. Maintain your core NextUI transpile configurations
   transpilePackages: [
     '@nextui-org/react',
     '@react-aria/i18n',
     'react-aria'
   ],
 
-  // 2. CORRECT ROOT LEVEL: Place this exactly here, completely outside of experimental!
-  serverExternalPackages: ['@react-aria/i18n', 'react-aria']
+  // 2. Clear out all old alias/experimental hacks and use this clean replacement rule
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(
+        new webpack.NormalModuleReplacementPlugin(
+          /@react-aria\/i18n\/dist\/import\.mjs$/,
+          require.resolve('@react-aria/i18n')
+        )
+      );
+    }
+    return config;
+  }
 };
 
 module.exports = nextConfig;
