a3ca294e578720ff4e69f0135840f0fecfa1722e
Annie He <anniezhe0@gmail.com>
2026-06-20 12:23:06 -0400

    Google Analytics setup finish

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import {Providers} from "./providers";
-
+import { GoogleAnalytics } from '@next/third-parties/google';
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
 export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
     <html lang="en" className='dark'>
       <body>
         <Providers>
           {children}
+          <GoogleAnalytics gaId={publicRuntimeConfig.GA_ID} />
         </Providers>
       </body>
     </html>
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,17 @@
 import * as React from "react";
 import { NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+import { pageview } from '@/lib/gtag';
 export function Providers({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (pathname) {
+      pageview(pathname);
+    }
+  }, [pathname]);
   return (
     <NextUIProvider>
       <NextThemesProvider
