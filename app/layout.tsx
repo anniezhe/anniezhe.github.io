688c6b672b11abcfebb3be9dc07a6f2eb25ac95f
Annie He <anniezhe0@gmail.com>
2026-06-20 12:47:35 -0400

    fix Google Analytics key

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,13 @@
 import {Providers} from "./providers";
 import { GoogleAnalytics } from '@next/third-parties/google';
-import getConfig from 'next/config';
-const { publicRuntimeConfig } = getConfig();
+
 export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
     <html lang="en" className='dark'>
       <body>
         <Providers>
           {children}
-          <GoogleAnalytics gaId={publicRuntimeConfig.GA_ID} />
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
         </Providers>
       </body>
     </html>
