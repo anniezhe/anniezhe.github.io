7b08d7704c9438aa433368813b0f81bbd12fd345
Annie He <anniezhe0@gmail.com>
2026-06-20 12:54:26 -0400

    fix gtag.ts

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,37 +1,11 @@
 // lib/gtag.ts
-import getConfig from 'next/config';
-
-const { publicRuntimeConfig } = getConfig();
-export const GA_ID = publicRuntimeConfig.GA_ID;
+export const GA_ID = process.env.NEXT_PUBLIC_GA_ID!
 
 // Send a pageview event to Google Analytics
 export const pageview = (url: string) => {
   if (typeof window !== 'undefined' && (window as any).gtag) {
     (window as any).gtag('config', GA_ID, {
       page_path: url,
-    });
-  }
-};
-
-/**
- * Send a custom GA event
- */
-export const event = ({
-  action,
-  category,
-  label,
-  value,
-}: {
-  action: string;
-  category: string;
-  label: string;
-  value?: number;
-}) => {
-  if (typeof window !== 'undefined' && (window as any).gtag && GA_ID) {
-    (window as any).gtag('event', action, {
-      event_category: category,
-      event_label: label,
-      value,
     });
   }
 };
