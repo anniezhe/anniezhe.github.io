625fef3399cb54c6178385e27c7aa942a07d2fc0
Annie He <anniezhe0@gmail.com>
2026-06-20 13:54:54 -0400

    add website's metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import {Providers} from "./providers";
 import { GoogleAnalytics } from '@next/third-parties/google';
 
+export const metadata = {
+  title: "Annie He's Website",
+  description: "Welcome to my website!",
+};
+
 export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
     <html lang="en" className='dark'>
